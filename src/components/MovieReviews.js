28eb45b033811c3e00ba12e4ac11d0ce38730622
bebopvinh <vinhdo07@gmail.com>
2019-06-29 02:08:40 -0700

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,30 @@
 // Code MovieReviews Here
+import React from 'react'
+
+// const renderMovies = (props) => {
+//    return props.reviews.map(
+//    review => {
+//       return <div className="review">
+//          <p>Movie Title: {review.display_title}</p>
+//          <p>By: {review.byline}</p>
+//       </div>
+//    }
+// )
+// }
+
+function myDebug (reviews) {
+   console.log(reviews)
+   if (reviews) return reviews.results.map(review => 
+      <div className="review">
+         <p>{review.display_title}</p>
+      </div>
+      )
+}
+
+const MovieReviews = ({reviews}) => {
+   return <div className="review-list">
+      {myDebug(reviews)}
+   </div>
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
+const NYT_API_KEY = 'EUNmC0UZCDuoKS4VAJgHT195WEPBJ6OD';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+   render() {
+      return (
+         <div>
+            
+         </div>
+      )
+   }
+}
