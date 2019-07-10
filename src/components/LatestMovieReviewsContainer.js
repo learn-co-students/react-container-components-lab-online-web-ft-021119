f47d18ba664d86f22018b19a65485a95cda86f17
kirstenwerner <kirstenmwerner@gmail.com>
2019-07-09 18:12:14 -0700

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,35 +2,32 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
 
-export default class LatestMovieReviewsContainer extends Component {
+    this.state = {
+      reviews: [{display_title: ""}]
+    }
+  }
 
+  componentDidMount() {
+    const NYT_API_KEY = 'EUNmC0UZCDuoKS4VAJgHT195WEPBJ6OD';
+    const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?' + `api-key=${NYT_API_KEY}`;
+    fetch(URL)
+      .then(resp => resp.json())
+      .then(reviews => {
+        this.setState({reviews: reviews})
+      })
+  }
 
-   constructor(props) {
-      super(props)
-
-      this.state = {
-         reviews: [{display_title: ""}]
-      }
-   }
-
-   componentDidMount() {
-      const NYT_API_KEY = 'EUNmC0UZCDuoKS4VAJgHT195WEPBJ6OD';
-      const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-         + `api-key=${NYT_API_KEY}`;
-      fetch(URL)
-         .then(resp => resp.json())
-         .then(reviews => {
-            this.setState({reviews: reviews})
-         })
-   }
-
-   render() {
-      return (
-         <div className="latest-movie-reviews">
-            <MovieReviews reviews={this.state.reviews} />
-         </div>
-      )
-   }
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
 }
+
+export default LatestMovieReviewsContainer
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,39 +2,40 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-// Code SearchableMovieReviewsContainer Here
-export default class SearchableMovieReviewsContainer extends Component {
-   constructor(props) {
-      super(props)
+class SearchableMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props)
 
-      this.state = {
-         reviews: [],
-         searchTerm: ""
-      }
-   }
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    }
+  }
 
-   handleSubmit = (event) => {
-      event.preventDefault()
-      const NYT_API_KEY = 'EUNmC0UZCDuoKS4VAJgHT195WEPBJ6OD';
-      const searchURL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}` + `api-key=${NYT_API_KEY}`;
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const NYT_API_KEY = 'EUNmC0UZCDuoKS4VAJgHT195WEPBJ6OD';
+    const searchURL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}` + `api-key=${NYT_API_KEY}`;
 
-      fetch(searchURL).then(resp => resp.json()).then(reviews => this.setState({ reviews }))
-   }
+    fetch(searchURL).then(resp => resp.json()).then(reviews => this.setState({ reviews }))
+  }
 
-   handleChange = (event) => {
-      this.setState({ [event.target.name]: event.target.value })
-   }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-   render() {
-      return (
-         <div className="searchable-movie-reviews">
-            <form onSubmit={this.handleSubmit}>
-               <input type="text" onChange={this.handleChange} name="searchTerm"/>
-               <input type="submit"/>
-            </form>
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} name="searchTerm"/>
+          <input type="submit"/>
+        </form>
 
-            <MovieReviews reviews={this.state.reviews}/>
-         </div>
-      )
-   }
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
 }
+
+export default SearchableMovieReviewsContainer
